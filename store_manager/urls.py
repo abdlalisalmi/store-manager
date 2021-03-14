f2eeab72a769ea4bbef0a251bd01f2616fc691d4
Abdelaali Es salmi <aes-salm@e2r10p6.1337.ma>
2021-03-14 11:01:02 +0100

    create home page and .gitignore file

--- store_manager/urls.py
+++ store_manager/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import homePage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("products.urls")),
+    path('', homePage, name='home'),
 ]
