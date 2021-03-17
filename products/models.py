f8e1d6efea898d8da30a2d409077723f6321de1c
Abdelaali Es salmi <aes-salm@e2r10p6.1337.ma>
2021-03-17 15:17:00 +0100

    comment compress image function

--- products/models.py
+++ products/models.py
@@ -31,19 +31,19 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
-    def save(self, *args, **kwargs):
-        self.image = self.compressImage(self.image)
-        super(Product, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.image = self.compressImage(self.image)
+    #     super(Product, self).save(*args, **kwargs)
 
-    def compressImage(self,uploadedImage):
-        imageTemproary = Image.open(uploadedImage)
-        if str(uploadedImage).split('.')[1].lower() == "png":
-            imageTemproary = imageTemproary.convert('RGB')
-        outputIoStream = BytesIO()
-        imageTemproary.save(outputIoStream , format='JPEG', quality=40) 
-        outputIoStream.seek(0)
-        uploadedImage = InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" % uploadedImage.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
-        return uploadedImage
+    # def compressImage(self,uploadedImage):
+    #     imageTemproary = Image.open(uploadedImage)
+    #     if str(uploadedImage).split('.')[1].lower() == "png":
+    #         imageTemproary = imageTemproary.convert('RGB')
+    #     outputIoStream = BytesIO()
+    #     imageTemproary.save(outputIoStream , format='JPEG', quality=40) 
+    #     outputIoStream.seek(0)
+    #     uploadedImage = InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" % uploadedImage.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
+    #     return uploadedImage
 
 # @receiver(post_delete, sender=Product)
 # def create_user_account(sender, instance, **kwargs):
