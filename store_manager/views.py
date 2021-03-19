7462367cf2ebe5b99664f3e9a05d1b2f30a52c91
Abdelaali Es salmi <aes-salm@e2r10p6.1337.ma>
2021-03-19 19:23:29 +0100

    delete test with 1000 products

--- store_manager/views.py
+++ store_manager/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render
 
-from products.models import Category, Product
-
 def homePage(request):
-    cat = Category.objects.get(pk=81)
-    for i in range(1000):
-        Product.objects.create(name=f"product {i}", category=cat, price=i, box_quantity=i * 2)
     return render(request, 'home.html', {})
