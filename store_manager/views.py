cb417fcde49fb9eb6b9b185a17b333a55839973b
Abdelaali Es salmi <aes-salm@e2r10p6.1337.ma>
2021-03-19 19:12:27 +0100

    test with 1000 products

--- store_manager/views.py
+++ store_manager/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from products.models import Category, Product
 
 def homePage(request):
+    cat = Category.objects.get(pk=82)
+    for i in range(1000):
+        Product.objects.create(name=f"product {i}", category=cat, price=i, box_quantity=i * 2)
     return render(request, 'home.html', {})
